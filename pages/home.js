f65683a39ccb218045295a5a1de13eb6da6da887
Lazontez <tezgardner1212@gmail.com>
2025-06-22 05:27:50 -0500

    Just IN Item

--- pages/home.js
+++ pages/home.js
@@ -1,7 +1,11 @@
 class HomePage{
     constructor(page){
-            
+        this.page = page
+        // This will be multiple items. Currently it will return the 15 'Just In' Items found on home page
+        this.justInItems = page.locator('.grid-product__content') 
     }
 
 
 }
+
+module.exports = {HomePage}
